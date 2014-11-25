86a6a7616025b47cc8865ffe9dbe64df0c45aa8a
geoffreyknopf777 <geoffreyknopf@gmail.com>
2014-11-24 20:42:57 -0800

    Server remote connections fixed

--- src/finalProject/GUI/game/GameUI.java
+++ src/finalProject/GUI/game/GameUI.java
@@ -34,7 +34,7 @@ public class GameUI {
 		frame.setLayout(null);
 		myGame.setJFrame(frame);
 		myGame.setPlayground(frame);
-		myGame.uClient = new Client("10.120.0.138");
+		myGame.uClient = new Client("10.121.3.192");
 		//10.120.22.60
 		for(int i=0; i<100; i++){
 			myGame.uClient.sendMsg("Hello Server, lovely day aint it?");
