8389c688247e8056180776ef07c2f03f37dcc3dd
geoffreyknopf777 <geoffreyknopf@gmail.com>
2014-12-01 18:40:11 -0800

    Debug Score Panel

--- src/finalProject/GUI/game/ScorePanel.java
+++ src/finalProject/GUI/game/ScorePanel.java
@@ -37,7 +37,9 @@ public class ScorePanel extends JPanel implements Runnable{
 		this.uRemainingTimeLabel = new JLabel("Remaining Time: 0:0");
 		this.uRemainingTimeLabel.setForeground(Color.white);
 		this.add(this.uRemainingTimeLabel);
-		this.add(new JLabel("Damage: "));
+		JLabel uDamageLabel = new JLabel("Damage: ");
+		uDamageLabel.setForeground(Color.white);
+		this.add(uDamageLabel);
 		this.add(this.uScoreLabel);
 		uPanel.add(this);
 		new Thread(this).start();
@@ -168,6 +170,8 @@ public class ScorePanel extends JPanel implements Runnable{
 				}
 				this.setSize(this.getWidth(), HEIGHT + vEnemyUsernames.size()*20);
 				
+				//
+				
 			// update remaining time
 				msg = Client.sendMsg("GET_GAME_ACTIVE " + nGameIndex);
 				if(msg.equals("GET_GAME_ACTIVE FAILURE")){
@@ -178,18 +182,18 @@ public class ScorePanel extends JPanel implements Runnable{
 				for (int i=0;i<3;i++) {
 					remainingTime = scan.next();
 				}
-				String min = remainingTime.split(":")[0];
-				String sec = remainingTime.split(":")[1];
-				// if remaining time is 0:0, stop this awesome time
-				if (Integer.parseInt(min)==0 && Integer.parseInt(sec)==0) {
-					try {
-						new scoreboard();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					this.uFrame.dispose();
-				}
+//				String min = remainingTime.split(":")[0];
+//				String sec = remainingTime.split(":")[1];
+//				// if remaining time is 0:0, stop this awesome time
+//				if (Integer.parseInt(min)==0 && Integer.parseInt(sec)==0) {
+//					try {
+//						new scoreboard();
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					this.uFrame.dispose();
+//				}
 				this.uRemainingTimeLabel.setText("Remaining Time: " + remainingTime);
 				scan.close();
 				
@@ -198,6 +202,7 @@ public class ScorePanel extends JPanel implements Runnable{
 			// check to see if the status is score board and navigate to the scoreboard:
 				//Check to see if the player is in an active game:
 				msg = Client.sendMsg("GET_PLAYER_STATUS");
+				System.out.println(msg);
 				uScan = new Scanner(msg);
 				uScan.next();
 				sStatus = uScan.next();
